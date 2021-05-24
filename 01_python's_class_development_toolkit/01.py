8af71474f32190277e3236a824d9fe0cd94144c4
roberto.rocuantv@gmail.com <roberto.rocuantv@gmail.com>
2021-05-23 22:37:46 -0400

    circles finished

--- 01_python's_class_development_toolkit/01.py
+++ 01_python's_class_development_toolkit/01.py
@@ -2,55 +2,54 @@ import math
 
 
 class Circle(object):
-	"An advanced circle analytic toolkit"
+    "An advanced circle analytic toolkit"
 
-	__slots__ = ['diameter']
-	version = "0.1.0"
+    __slots__ = ['diameter']
+    VERSION = '0.1'
 
+    def __init__(self, radius):
+        self.radius = radius
 
-	def __init__(self, radius):
-		self.radius = radius
+    def area(self):
+        "Perform quadrature on a shape of uniform radius"
+        # return math.pi * self.radius ** 2.0
+        p = self.perimeter()
+        r = p / math.pi / 2.0
+        return math.pi * r ** 2.00
 
+    def perimeter(self):
+        return 2.0 * math.pi * self.radius
+    
+    # _perimeter = perimeter
 
-	def area(self):
-		"Perform quadrature on a shape of uniform radius"
-		# return math.pi * self.radius ** 2.0
-		p = self.perimeter()
-		r = p / math.pi / 2.0
-		return math.pi  * r ** 2.0
+    @property
+    def radius(self):
+        "Radius of a circle"
+        return self.diameter / 2.0
 
+    @radius.setter
+    def radius(self, radius):
+        self.diameter = radius * 2.0
 
-	def perimeter(self):
-		return 2.0 * math.pi * self.radius
+    @classmethod
+    def from_bbd(cls, bbd):
+        "Construct a circle from a bounding box diagonal"
+        radius = bbd / 2.0 / math.sqrt(2.0)
+        # return Circle(radius)
+        return cls(radius)
 
-
-	@property
-	def radius(self):
-		"Radius of a circle"
-		return self.diameter / 2.0
-
-	@radius.setter
-	def radius(self, radius):
-		self.diameter = radius * 2.0
-
-
-	@staticmethod
-	def angle_to_grade(angle):
-		"Convert angle in degree to a percentage grade"
-		return math.tan(math.radians(angle)) * 100.0
-
-
-	@classmethod
-	def from_bdd(cls, bdd):
-		"Construct a circle from a bounding box diagonal"
-		radius = bdd / 2.0 / math.sqrt(2.0)
-		return Circle(radius)
+    @staticmethod
+    def angle_to_grade(angle):
+        "Convert angle in degree to a percentage grade"
+        return math.tan(math.radians(angle)) * 100.0
 
 
+# Instance Tire again to get angle_to_grade staticmethod
 class Tire(Circle):
-	"Tires are circle with a corrected perimeter"
+    "Tires are circles with a corrected perimeter"
 
-
-	def perimeter(self):
-		"Circumference corrected for the rubber"
-		return Circle.perimeter(self) * 1.25
+    def perimeter(self):
+        "Circumference corrected for the rubber"
+        return Circle.perimeter(self) * 1.25
+    
+    # _perimeter = perimeter
